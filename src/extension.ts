674167fd84854de7fe565015cbd802660d97aaf8
1natsu172 <1natsu172@users.noreply.github.com>
2019-05-12 23:22:13 +0900

    feat: implement language-specific behavior

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,67 @@
-'use strict';
+'use strict'
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import { TextDocument } from 'vscode'
 
-const LINE_BREAK_TAG = `<br>`
+const EXTENSION_ID = 'insert-br-tag'
+
+const lineBreakTag = () => {
+  const editor = vscode.window.activeTextEditor
+  const extensionConfig = vscode.workspace.getConfiguration(
+    EXTENSION_ID,
+    editor.document.uri
+  )
+  const currentLanguageId = editor.document.languageId
+
+  const trailingSlashLanguageList = extensionConfig.get(
+    'trailingSlashLanguage'
+  ) as string[]
+
+  const tag = trailingSlashLanguageList.includes(currentLanguageId)
+    ? `<br />`
+    : `<br>`
+
+  return tag
+}
+
+const isEnableOnLanguage = (
+  enableLanguageList: TextDocument['languageId'][],
+  languageId: TextDocument['languageId']
+) => enableLanguageList.includes(languageId)
 
 function insertEditorSelection(text: string) {
-  const editor = vscode.window.activeTextEditor;
-  const selections = editor.selections;
-  
-  editor.edit((editBuilder) => {
-    selections.forEach((selection) => {
-      editBuilder.insert(selection.active, text);
-    });
-  });
+  const editor = vscode.window.activeTextEditor
+  const extensionConfig = vscode.workspace.getConfiguration(
+    EXTENSION_ID,
+    editor.document.uri
+  )
+  const currentLanguageId = editor.document.languageId
+  const enableLanguageList = extensionConfig.get('enableOnLanguage') as string[]
+
+  if (!isEnableOnLanguage(enableLanguageList, currentLanguageId)) return
+
+  const selections = editor.selections
+
+  editor.edit(editBuilder => {
+    selections.forEach(selection => {
+      editBuilder.insert(selection.active, text)
+    })
+  })
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  let insertLineBreak = vscode.commands.registerCommand(
-    'insertHTMLTag.insertLineBreak',
-    () => { insertEditorSelection(LINE_BREAK_TAG) }
+export async function activate(context: vscode.ExtensionContext) {
+  const insertLineBreak = vscode.commands.registerCommand(
+    `${EXTENSION_ID}.insertToSelection`,
+    () => {
+      insertEditorSelection(lineBreakTag())
+    }
   )
 
-  context.subscriptions.push(insertLineBreak);
+  context.subscriptions.push(insertLineBreak)
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
